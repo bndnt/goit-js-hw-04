913a8135d30b39f17867b485dfcfe0e908d0b4e6
bndnt <67003594+bndnt@users.noreply.github.com>
2024-04-04 13:24:59 -0400

    coded with love

---
+++ js/task-3.js
@@ -0,0 +1,31 @@
+console.log("Task 3 %s", "🔥");
+
+const profile = {
+  username: "Jacob",
+  playTime: 300,
+  changeUsername(newName) {
+    this.username = newName;
+  },
+  updatePlayTime(hours) {
+    this.playTime += hours;
+  },
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
+};
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
+
+const style = [
+  "background: #",
+  "color: #FF0000",
+  "padding: 10px 20px",
+  "line-height: 35px",
+].join(";");
+console.log("%c Coded with ♥️", style);
